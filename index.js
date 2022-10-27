ad466d540c243a6c2fdefd391d8bfa0b64b2f514
AnamikaSa <amanika0061@gmail.com>
2022-10-27 19:06:57 +0600

    for id 6 titile added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express();
-// const cors=require('cors');
+const cors=require('cors');
 const port = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(cors());
 
 const coursesCatagory =require('./data/coursesCatagory.json');
 const courses=require('./data/courses.json');
@@ -16,7 +16,12 @@ app.get('/categories', (req, res) => {
     res.send(coursesCatagory)
 });
 
-
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+        const category = courses.filter(c => c.category_id === id);
+        res.send(category);
+    }
+)
 
 
 app.get('/courses', (req, res) =>{
@@ -25,7 +30,7 @@ app.get('/courses', (req, res) =>{
 
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
-    const selectedCourses = courses.find(n => n._id === id);
+    const selectedCourses = courses.find(c => c._id === id);
     res.send(selectedCourses);
 });
 
